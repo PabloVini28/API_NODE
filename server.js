7e58ff81bfb46d9647bbe1de306f83b3c01163c9
Pablo Vinas <pablo.araujo@alu.ufc.br>
2024-11-08 12:48:01 -0300

    nao sei

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-
+console.log("aaa");
 server.listen(port);
